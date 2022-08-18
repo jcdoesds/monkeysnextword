343f264d6b54063172704d299308b7c5d484cc33
jcdoesds <71142698+jcdoesds@users.noreply.github.com>
2022-08-18 09:09:24 -0700

    Add files via upload
    updates to working version

--- monkeysnextwordapp/server.R
+++ monkeysnextwordapp/server.R
@@ -6,20 +6,266 @@
 
 library(shiny)
 
-# Define server logic required to draw a histogram
+library(tidyverse)
+
+# read in the words data from the blogs and news
+f_dir <- "https://raw.githubusercontent.com/jcdoesds/monkeysnextword/main/monkeysnextwordapp/data/"
+top_words <- read_csv(paste0(f_dir, "top_words.csv")) # three words with n
+# these are all structured moosh out n
+words2 <- read_csv(paste0(f_dir, "words2.csv"))
+words3 <- read_csv(paste0(f_dir, "words3.csv"))
+words4 <- read_csv(paste0(f_dir, "words4.csv"))
+words5 <- read_csv(paste0(f_dir, "words5.csv"))
+words6 <- read_csv(paste0(f_dir, "words6.csv"))
+
+num_words_input <- 0
+num_words_used <- 0
+num_matches <- 0
+
+cleanText <- function(this_text) {
+  this_text <- tolower(this_text) # make all lower case
+  # we will be removing punctuation, but maybe handle contractions first?
+  this_text <- gsub("'s", "", this_text) #not sure how to treat 's
+  this_text <- gsub("won't", "will not", this_text)
+  this_text <- gsub("can't", "can not", this_text)
+  this_text <- gsub("n't", " not", this_text) #special cases handled first
+  this_text <- gsub("'ll", " will", this_text)
+  this_text <- gsub("'re", " are", this_text)
+  this_text <- gsub("'ve", " have", this_text)
+  this_text <- gsub("'m", " am", this_text)
+  this_text <- gsub("'d", " would", this_text)
+  this_text <- gsub("http[^[:space:]]*", "", this_text) # get rid of urls
+  this_text <- gsub("@[^\\s]+", "", this_text) # handle twitter names
+  this_text <- gsub("[^[:alpha:][:space:]]*", "", this_text) # get rid of special char
+  this_text <- gsub("[[:punct:]]", "", this_text) # get rid of all other punctuation
+  return(this_text)
+}
+
+returnWordsWithOdds <- function(df, tomatch) {
+  # take the df, see if moosh has a match
+  # moosh is a combination of all but the last word, which is out
+  
+  # will return a df that is empty or has up to ten
+  # words with odds
+  
+  df <- df %>% filter(moosh == tomatch) 
+  
+ num_matches <<- nrow(df)
+  
+  df %>%
+    mutate(odds = n/sum(n)) %>% 
+    top_n(10,n) %>%
+    select(out, odds)
+  
+}
+
+getNextLikelyWords <- function(input_words, grams = 4){
+  num <- length(input_words)
+  snum <- num - grams + 2 # ensures only enough words
+  ## PREPARE THE GLOBALS
+  num_words_input <<- num
+  num_words_used <<- grams - 1
+  ## END OF GLOBALS
+  tomatch <- str_flatten(input_words[snum:num], "_")# smoosh together the grams - 1 last words of input
+  # each df is word1-wordn, n, proportion
+  # based on number of grams, pick data
+  # if match, filter to matches, calc odds
+  matchdf <- get(paste0("words",grams)) # words2, words3, wordsn
+  next_words <- returnWordsWithOdds(matchdf, tomatch)
+  #print("this time matched")
+  #print(next_words)
+  if(nrow(next_words) == 0 & grams > 2){
+    # we don't have any hits, try again with fewer grams
+    getNextLikelyWords(input_words, grams = grams - 1)
+  } else {
+    # we have some hit
+    return(next_words)
+  }
+}
+
+getNextLikelyFromInput <- function(input){
+  if(is.na(input) || input == "" || input == " "){
+    return(top_words) # get out
+  } else {
+    input <- cleanText(input)  # THIS IS WHERE WE CLEAN IT TO MATCH!!!
+    # Find word count, separate words, lower case
+    input_count <- str_count(input, boundary("word"))
+    input_words <- unlist(str_split(input, boundary("word")))
+    input_words <- tolower(input_words)
+
+    
+    result <- getNextLikelyWords(input_words, min(input_count + 1, 6)) # max 6
+    if(is.null(result) | length(result) == 0 | nrow(result) == 0) {
+      result <- top_words
+      num_words_used <<- 0 # it did not match on any of their words
+      #print("no result: using top words")
+    } else{
+      if(nrow(result) < 3){
+        #print(nrow(result))
+        #print("not enough result: adding top words")
+        # use filter to make sure only give a result one time
+        result <- bind_rows(result, filter(top_words, !out %in% result$out)) %>%
+          ungroup() %>%
+          mutate(odds = odds/sum(odds)) %>% 
+          top_n(10,odds)
+        
+        #print(result)
+      }
+    }
+    return(result)
+  }
+  
+}
+
+
+# Define server logic required to return words
 shinyServer(function(input, output) {
+  
+  words_react <- reactive({
+    if(is.null(input$user_entry)){
+      getNextLikelyFromInput("")
+    } else {
+      getNextLikelyFromInput(input$user_entry)
+    }
+  })
+  
+   output$likely_words <- renderText({
+     paste(top_words$word)
+   })
+   
+   output$likely1 <- renderText({
+     w <- words_react()
+     if(grepl("monkey", input$user_entry)){
+       "\u2764"
+     } else { # only go here if they didn't say monkey}
+     if(nrow(w) > 0){
+       if(num_matches == 0){"???"} else { w$out[1] }
+     }
+     else ("")
+     }
+   })
+   
+   output$likely2 <- renderText({
+     w <- words_react()
+     if(grepl("monkey", input$user_entry)){
+       "\u2764 \u2764"
+     } else { # only go here if they didn't say monkey}
+     if(nrow(w) > 1){
+       if(num_matches == 0){"???"} else { w$out[2] }
+     }
+     else ("")
+     }
+   })
+   output$likely3 <- renderText({
+     w <- words_react()
+     if(grepl("monkey", input$user_entry)){
+       "\u2764 \u2764 \u2764"
+     } else { # only go here if they didn't say monkey}
+       if(nrow(w) > 2){
+         if(num_matches == 0){"???"} else { w$out[3] }
+       }
+       else ("")
+     }
+   })
+   
+   output$easter_egg <- renderText({
+     w <- words_react()
+     if(grepl("monkey", input$user_entry)){
+       "\u2764   You typed the magic word, 'M-O-N-K-E-Y' and made the monkeys so happy!   \u2764"
+     } else { # only go here if they didn't say monkey}
+       ""
+     }
+   })
+   
+   output$num_matches <- renderText({
+     w <- words_react()
+     if(num_matches == 0){
+       ""
+     } else { 
+       o <- paste("There were", format(num_matches,big.mark=",", trim=TRUE), "matches.")   
+       if(num_matches < 3){
+         o <- paste(o, "Since there were fewer than three matches to your typing, 
+                    the monkeys have additionally given a few examples of 
+                    the most common single words. These words may not make much
+                    sense in your context, but hey, they are only monkeys.")
+       }
+       o
+     }
+   })
+   
+   output$num_words_input <- renderText({
+     w <- words_react()
+     if(num_words_input == 0){
+       ""
+     } else { 
+       paste("Monkeys see that you input", 
+             format(num_words_input,big.mark=",", trim=TRUE), "words.")   
+     }
+   })
+   
+   output$num_words_used <- renderText({
+     w <- words_react()
+     if(num_words_input == 0) {
+       ""
+     } else {
+       if(num_matches == 0){
+         "They did not find any matches, and are only showing you the most likely words."
+       } else { 
+         paste("They used",
+               format(num_words_used,big.mark=",", trim=TRUE),
+               "of them to determine your next likely words.")
+       }}
+   })
+   
+   
 
-    output$distPlot <- renderPlot({
+   
+   output$likely_graph <- renderPlot({
+     w <- words_react()
+     if(num_words_input == 0) {
+       # we don't want a graph here
+     } else {
+     w$out <- forcats::fct_inorder(w$out) %>% forcats::fct_rev() #keep colors right
+     # get only the clors we need 
+     # use the min 10 function to ensure that don't have too many if issue w 
+     mcolors <- tail(c(rep("grey50",7),"#ea526f","#ff8a5b","#f0F2A6"), min(10,nrow(w)))
+  
+     ggplot(head(w,10), aes(x = out, y = odds, fill = out, color = out)) +
+       geom_col()+
+       geom_text(aes(label = sprintf("%1.1f%%", 100*odds)), 
+                 hjust = 0, 
+                 nudge_y = 0.001, fontface = "bold",
+                 size = rel(5)) +
+       scale_y_continuous(labels = scales::percent_format(accuracy = 1),
+                          limits = c(0,w$odds[1]*1.25)) +
+       scale_color_manual(values = mcolors) +
+       scale_fill_manual(values = mcolors) +
+       labs(x = "", y="", title = "Odds It Is Your Word") +
+       coord_flip() +
+       theme_minimal() +
+       theme(
+         legend.position = "none",
+         plot.title = element_text(size = rel(2), color = "white"),
+         axis.text.y= element_text(size=rel(3)),
+         axis.text.x=element_blank(),
+         axis.ticks=element_blank(),
+         panel.background = element_rect(fill = "transparent",
+                                         colour = NA_character_), # necessary to avoid drawing panel outline
+         panel.grid.major = element_blank(), # get rid of major grid
+         panel.grid.minor = element_blank(), # get rid of minor grid
+         plot.background = element_rect(fill = "transparent",
+                                        colour = NA_character_), # necessary to avoid drawing plot outline
+         legend.background = element_rect(fill = "transparent"),
+         legend.box.background = element_rect(fill = "transparent"),
+         legend.key = element_rect(fill = "transparent")
+       )
+     } # end of if something statement
+   }, bg="transparent")
+   
+   output$your_words <- renderText({
+     paste("You said",input$user_entry)
+   })
 
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-
-    })
+    
 
 })
--- monkeysnextwordapp/ui.R
+++ monkeysnextwordapp/ui.R
@@ -9,37 +9,205 @@
 
 library(shiny)
 
+mystyle <- '
+  @import url("https://fonts.googleapis.com/css2?family=Rubik+Marker+Hatch&display=swap");
+  .myclass { 
+  background-color: #25CED1;
+  color: white;
+  }
+
+  .mytitle{
+  text-shadow: 2px 2px #666a86;
+  text-align: center;
+  padding: 30px;
+  font-size: 3em;
+  font-family: "Rubik Marker Hatch", sans-serif;
+  font-weight: bold;
+  }
+  .mymain {
+  background-color:  #25CED1;
+  color: black;
+  text-align:center;
+  }
+  .myintrotext{
+  color: #383838;
+  text-align: left;
+  padding: 0px 30px;
+  
+  }
+  .mydesc{
+  color: #383838;
+  text-align: left;
+  padding: 50px 30px 0px 30px;
+  }
+  .myfooter { 
+  background-color: #666a86;
+  color: #E6E6FA;
+  }
+  .btn {
+  background-color: #666a86;
+  color: #E6E6FA;
+  font-size: 2em;
+  padding: 20px;
+  }
+  .myform {
+  
+  padding: 20px;
+  color:gray;
+  text-align:center;
+  width: "100%";
+  }
+ #user_entry.form-control{
+ font-size: 2em;
+ color:gray;
+ padding: 20px;
+ width: "100%";
+ }
+  .monkeytext {
+    margin: 250px 0px 5px 0px;  
+  }
+  
+  
+  .monkey1{
+   background-color: #f0F2A6;
+   background-image: url("img/monkey1.png");
+   background-size:     cover;                  
+    background-repeat:   no-repeat;
+    background-position: center center;  
+   height:300px;
+   text-align:center;
+  }
+  .monkey2{
+   background-color: #ff8a5b;
+   background-image: url("img/monkey2.png");
+   background-size:     cover;                  
+    background-repeat:   no-repeat;
+    background-position: center center;  
+   color: #E6E6FA;
+   height:300px;
+   text-align:center;
+  }
+  .monkey3{
+   background-color: #ea526f;
+   background-image: url("img/monkey3.png");
+   background-size:     cover;                  
+    background-repeat:   no-repeat;
+    background-position: center center;  
+   color: white;
+   height:300px;
+    text-align:center;
+  }
+  .monkeysum{
+  height:300px;
+  }
+'
+
 
 shinyUI(
-  fluidPage(
+  fluidPage(tags$head(tags$style(mystyle)),
+            class = "myclass",
     
     title = "Monkeys Guess Your Next Word",
     
-    fluidRow(
-      column(5, offset = 4,
-             h1("Monkeys Guess The Next Word")
+    fluidRow(column(12, class = "mytitle",
+             
+        h1("Monkeys Guess The Next Word")
       )
     ),
     
-    plotOutput("distPlot"),
     
     hr(),
     
-    fluidRow(
-      column(4,
-             h4("Diamonds Explorer")
-      ),
-      column(4,
-             sliderInput("bins",
-                         "Number of bins:",
-                         min = 1,
-                         max = 50,
-                         value = 30)
-      ),
-      column(4,
-             h4("right")
+    fluidRow(column(12, class="mymain col-md-6 col-lg-6",
+            fluidRow(h2("Not sure what to type?")),
+            fluidRow(class="myintrotext",
+                     h4("We found some monkeys that can show you likely next words."),
+                     h4("Well, actually, they are trained to show you the most likely words from a list that an algorithm finds based on what you type."),
+                     h4("Go a head and try typing something in the box. Then hit 'Guess My Word'")
+                     ) ,
+            br(),
+            fluidRow( class="myform",
+              column(6,
+              textInput("user_entry","Your Words")
+              ),
+              column(6, 
+              submitButton(text = "Guess My Word")
+              )
+              ),
+            br(),
+            fluidRow(column(12, class="mydesc",
+               p(textOutput("num_words_input", inline=T),
+                 textOutput("num_words_used", inline=T),
+                 textOutput("num_matches", inline=T),
+                 textOutput("easter_egg", inline=T))
+              ))
+            ), # end of left side
+            column(12, class="mymonkeys col-md-6 col-lg-6",
+               fluidRow(
+                   column(6, class="monkey1",
+                          
+                          h1(class = "monkeytext",
+                             textOutput("likely1"))
+                   ),
+                   column(6, class="monkey2",
+                          h1(class = "monkeytext",
+                             textOutput("likely2"))
+                   )
+                   ),
+               fluidRow(
+                 column(6, class="monkey3",
+                            h1(class = "monkeytext",
+                                  textOutput("likely3"))
+                            
+                        
+                 ),
+                 column(6, class="monkeysum",
+                       plotOutput("likely_graph", width = "100%", height = "100%")
+                 )
+               ),
+            )
+    
+      
+    ),
+
+    
+
+    
+    hr(),
+    
+    fluidRow(align = "center", class="myfooter",
+      h2("About This App"),
+      h4("The goal of this app is to guess the next word based on a 
+         backoff model tuned using text from blogs and news. To keep the app 
+         size small, it is a very simple model. The backoff model starts with 
+         consideration for up to 5 input words. If there are more than 5 words
+         input, it starts with the last words. If there is no match based on the 
+         5 words input, it looks at the next smaller set of words. If there is 
+         still no match looking at only the last word, it gives the most common words.
+         In testing, the model can be improved slightly with topic and sentiment 
+         analysis. So, if the input words include 'animals', it would first look for
+         matching words in a subset of the data with the topic 'animals', and if
+         the input words were 'negative' or 'positive' in tone, it would further
+         look for similar sentiment in the training data."),
+      br(),
+      h2("App"),
+      h4("The app is written in RShiny. 
+         Code by jcdoesds in RStudio. 
+         Art by jcdoesds in ProCreate for iPad.
+         ~ Did you find the Easter Egg ~"),
+      br(),
+      h2("Data"),
+      h4("The data are based on a sample of 30% of all of the lines of data provided 
+         in the JHU Data Science Capstone for the blogs and news in english. From the
+         sample, lines were cleaned and tokenized into ngrams of up to six words. To make 
+         this shiny app load more quickly, the top ten single words in the sample, and
+         data frames containing the ngrams were saved as .csv and used here rather than
+         build them directly from the source for this app. Data can be found in the github directory"),
+      br(),
+      h2("Author"),
+      h4("jcdoesds loves data science, exploration, analysis, and visualization.")
       )
-    )
+    
   )
 )
 
